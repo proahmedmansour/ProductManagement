a6543befa4b6c81e767fb7e475c021bcedd8e597
Ahmed Manssur <AhmedManssur@outlook.com>
2022-04-15 14:27:32 +0200

    Added get product list in application service with automation test.

--- src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
+++ src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ProductManagement.Products;
 
 namespace ProductManagement;
 
@@ -6,8 +7,6 @@ public class ProductManagementApplicationAutoMapperProfile : Profile
 {
     public ProductManagementApplicationAutoMapperProfile()
     {
-        /* You can configure your AutoMapper mapping configuration here.
-         * Alternatively, you can split your mapping configurations
-         * into multiple profile classes for a better organization. */
+        CreateMap<Product, ProductDto>();
     }
 }
